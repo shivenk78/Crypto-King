5746d81365b835da339131c1e444d41756082d58
Shiven <kumar.shiven15@gmail.com>
2018-02-26 22:24:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/shiven/bloombergproj/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.shiven.bloombergproj;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFrag());
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+            getFragmentManager().beginTransaction().replace(R.id.id_fragment_frame, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.navigation_home:
+                fragment = new HomeFrag();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFrag();
+                break;
+            case R.id.navigation_trading:
+                fragment = new TradingFrag();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+}
---
+++ app/src/main/java/com/example/shiven/bloombergproj/TradingFrag.java
@@ -0,0 +1,21 @@
+package com.example.shiven.bloombergproj;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shiven on 2/26/2018.
+ */
+
+public class TradingFrag extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_trading, null);
+    }
+}
